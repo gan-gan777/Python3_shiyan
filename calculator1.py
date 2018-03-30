f1d67331c6e49c7471d6a4a4bb2805fad5811a2e
gan-gan777 <shot_gan@hotmail.com>
2018-03-30 16:36:29 +0800

    Update calculator1.py
    计算处理多人的税前工资，并将计算后的税后工资打印出来。

--- calculator1.py
+++ calculator1.py
@@ -1,56 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-# 2018-03-24
+# 2018-03-30
 # Personal Tax Calculator
 
 import sys    # 导入系统模块
+insurance = float(0.165)    # 五险一金占工资比例
+TT = int(3500)    # 纳税起征点金额
 
 # 个税计算公式：
 # 1. 应纳税所得额 = 工资金额 - 各项社会保险 - 起征点（3500元）
 # 2. 应纳税额 = 应纳税所得额 * 税率 - 速算扣除数
-# 公式1中的“各项社会保险费”暂不考虑。
 
 # 英语单词翻译：
-# Salary：薪水；
+# Wages：工资；
+# Pre-tax wages：税前工资，简写为“Pre_wages”；
+# After-tax wages：税后工资，简写为“Aft_wages”；
+# Insurance：保险税率；
+# Rapid：速算扣除数；
+# Tax_rate：税率；
 # Taxable Income：应纳税所得额，简写为“TI”；
 # Tax Payable：应纳税额，简写为“TP”；
 # Tax Threshold：个税起征点,简写为“TT”；
 
-# 异常处理：用户输入的参数必须为单个的、正确的工资数额！参数数量不准确、输入为负数、输入内容无法转成整数型，都需要打印参数错误提示。
-try:
-    len(sys.argv) == 2    # 参数个数必须为2（连同参数名称在内）
-    Salary = int(sys.argv[1])    # 输入的“工资字符”必须能转成整型 
-    assert Salary > 0    # 输入工资必须>0
-except ValueError:
-    print('数据类型错误')
-    exit()
-except AssertionError:
-    print('输入工资必须>0')
-    exit()
+# 税后工资计算公式
+def furmula(Pre_wages, rapid, tax_rate)
+    try:
+        ins_amount = Pre_wages * insurance    # 五险一金费用总额
+        TI = Pre_wages - ins_amount - TT    # 应纳税所得额计算公式
+        TP = TI * tax_rate - rapid    # 应纳税额计算公式
+        Aft_wages = format((Pre_wages - ins_amount - TP), ".2f")    # 税后工资计算公式
+        return Aft_wages
+    except:
+        print("参数错误")
 
-TT = 3500    # 个税起征点数值；
-TI = int(Salary) - TT    # 应纳税所得额公式；
+# 多员工工资计算
+def Calc(id, Aft_wages)
+    TI = Pre_wages - ins_amount - TT
+    Aft_wages_dict = {}
+    if TI <= 0:
+        Aft_wages = furmula(Pre_wages, 0, 0)
+    elif TI > 0 and <= 1500:
+        Aft_wages = furmula(Pre_wages, 0, 0.03)
+    elif TI > 1500 and TI <= 4500:
+        Aft_wages = furmula(Pre_wages, 105, 0.1)
+    elif TI > 4500 and TI <= 9000:
+        Aft_wages = furmula(Pre_wages, 555, 0.2)
+    elif TI > 9000 and TI <= 35000:
+        Aft_wages = furmula(Pre_wages, 1005, 0.25)
+    elif TI > 35000 and TI <= 55000:
+        Aft_wages = furmula(Pre_wages, 2755, 0.3)
+    elif TI > 55000 and TI <=80000:
+        Aft_wages = furmula(Pre_wages, 5505, 0.35)
+    elif TI > 80000:
+        Aft_wages = furmula(Pre_wages, 13505, 0.45)
+    print(id, end='')
+    print(':', end='')
+    print(Aft_wages)
 
-if TI <= 0:
-    print(format(0,".2f"))
-elif TI <= 1500 and TI > 0:
-    TP = format((TI * 0.03 - 0),".2f")
-    print(TP)
-elif TI > 1500 and TI <= 4500:
-    TP = format((TI * 0.1 - 105),".2f")
-    print(TP)
-elif TI > 4500 and TI <= 9000:
-    TP = format((TI * 0.2 - 555),".2f")
-    print(TP)
-elif TI > 9000 and TI <= 35000:
-    TP = format((TI * 0.25 - 1005),".2f")
-    print(TP)
-elif TI > 35000 and TI <= 55000:
-    TP = format((TI * 0.30 - 2755),".2f")
-    print(TP)
-elif TI > 55000 and TI <=80000:
-    TP = format ((TI * 0.35 - 5505),".2f")
-    print(TP)
-elif TI > 80000:
-    TP = format((TI * 0.45 - 13505),".2f")
-    print(TP)
+# 用户传参
+for arg in sys.argv[1:]:
+    deployee_list = arg.split(':')
+    calc(int(deployee_list[0]), int(deployee_list[1]))
