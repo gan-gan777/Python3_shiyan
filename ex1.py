20056fe78aaf1dc3bbb772a2818b223ecba97349
shot_gan <shot_gan@hotmail.com>
2018-04-02 17:48:45 +0800

    modified a file

--- ex1.py
+++ ex1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-class Dog(object):    # 定义
+class Dog(object):    # 创建一个狗狗类
     def __init__(self, name):    # 定义__init__方法，这里有两个参数。self指的是被创建类的实例本身。
         self._name = name    # 实例的“_name”属性对应__init__的参数"name"(01为啥加下划线？！)
     def get_name(self):    # 02不明白？
@@ -23,5 +23,5 @@ class Cat(object):
 dog = Dog('旺财')    # 创建Dog类的实例
 cat = Cat('Kitty')
 
-dog.bark()    # 不明白？
+dog.bark()    # 06不明白？
 cat.mew()
